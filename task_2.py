f183c9a43b5fdbcd53b21658ef2df97c8f865f1a
truart <trubinart@gmail.com>
2020-10-24 18:05:21 +0300

    hw_7 done

---
+++ task_2.py
@@ -0,0 +1,52 @@
+"""
+2. Отсортируйте по возрастанию методом слияния одномерный вещественный массив,
+заданный случайными числами на промежутке [0; 50). Выведите на экран исходный
+и отсортированный массивы.
+
+Пример:
+Введите число элементов: 5
+Исходный - [46.11436617832828, 41.62921998361278, 18.45859540989644, 12.128870723745806, 8.025098788570562]
+Отсортированный - [8.025098788570562, 12.128870723745806, 18.45859540989644, 41.62921998361278, 46.11436617832828]
+"""
+import numpy
+import copy
+
+orig_list = [numpy.random.uniform(0, 50) for _ in range(10)]
+orig_list_2 = copy.deepcopy(orig_list)
+print(orig_list)
+
+def merge_sort(lst_obj):
+    if len(lst_obj) > 1:
+        center = len(lst_obj) // 2
+        left = lst_obj[:center]
+        right = lst_obj[center:]
+
+        merge_sort(left)
+        merge_sort(right)
+
+        # перестали делить
+        # выполняем слияние
+        i, j, k = 0, 0, 0
+
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                lst_obj[k] = left[i]
+                i += 1
+            else:
+                lst_obj[k] = right[j]
+                j += 1
+            k += 1
+
+        while i < len(left):
+            lst_obj[k] = left[i]
+            i += 1
+            k += 1
+
+        while j < len(right):
+            lst_obj[k] = right[j]
+            j += 1
+            k += 1
+        return lst_obj
+
+merge_sort(orig_list_2)
+print(orig_list_2)
---
+++ task_3.py
@@ -0,0 +1,42 @@
+"""
+3. Массив размером 2m + 1, где m – натуральное число, заполнен случайным образом.
+Найдите в массиве медиану. Медианой называется элемент ряда, делящий его на
+две равные части: в одной находятся элементы, которые не меньше медианы,
+в другой – не больше медианы.
+
+Задачу можно решить без сортировки исходного
+массива.
+
+Но если это слишком сложно, то используйте метод сортировки,
+который не рассматривался на уроках: Шелла, Гномья, ...
+
+arr[m]
+from statistics import median
+"""
+from statistics import median
+import random
+import copy
+
+
+# m = int(input('Введите m: '))
+orig_list = [random.randint(-100, 100) for _ in range((2*2)+1)]
+print(orig_list)
+
+#элемент больше len // 2 элементов
+i = 0
+for num in orig_list:
+    for num_2 in orig_list:
+        if num > num_2:
+            result = num
+            i += 1
+    if i == 2:
+        result = num
+        break
+    i = 0
+
+print(f'Медиана моего кода {result} \n')
+
+orig_list.sort()
+print(orig_list)
+print(f'Медиана модуля MEDIAN {median(orig_list)}')
+
