e1176e3c078ea9988e1d006c5318e6c95f1570f8
truart <trubinart@gmail.com>
2020-10-29 17:24:31 +0300

    done-2

--- task_2.py
+++ task_2.py
@@ -13,61 +13,36 @@
 class BinaryTree:
     def __init__(self, root_obj):
         self.root = root_obj
-        # левый потомок
         self.left_child = None
-        # правый потомок
         self.right_child = None
 
-    # добавить левого потомка
     def insert_left(self, new_node):
-
-        # # если у узла нет левого потомка
-        # if self.left_child == None:
-        #     # тогда узел просто вставляется в дерево
-        #     # формируется новое поддерево
-        #     self.left_child = BinaryTree(new_node)
-        # # если у узла есть левый потомок
         if new_node < self.root or self.left_child == None:
-            # тогда вставляем новый узел
             tree_obj = BinaryTree(new_node)
-            # и спускаем имеющегося потомка на один уровень ниже
             tree_obj.left_child = self.left_child
             self.left_child = tree_obj
         else:
             print('Нельзя вставлять влево число БОЛЬШЕ корня')
             self.insert_right(new_node)
 
-    # добавить правого потомка
     def insert_right(self, new_node):
-        # # если у узла нет правого потомка
-        # if new_node > self.root or self.right_child == None:
-        #     # тогда узел просто вставляется в дерево
-        #     # формируется новое поддерево
-        #     self.right_child = BinaryTree(new_node)
-        # # если у узла есть правый потомок
         if new_node > self.root or self.right_child == None:
-            # тогда вставляем новый узел
             tree_obj = BinaryTree(new_node)
-            # и спускаем имеющегося потомка на один уровень ниже
             tree_obj.right_child = self.right_child
             self.right_child = tree_obj
         else:
             print('Нельзя вставлять влево число МЕНЬШЕ корня')
             self.insert_left(new_node)
 
-    # метод доступа к правому потомку
     def get_right_child(self):
         return self.right_child
 
-    # метод доступа к левому потомку
     def get_left_child(self):
         return self.left_child
 
-    # метод установки корня
     def set_root_val(self, obj):
         self.root = obj
 
-    # метод доступа к корню
     def get_root_val(self):
         return self.root
 
@@ -82,6 +57,7 @@ print(r.get_right_child().get_root_val())
 
 
 r.insert_right(12)
+r.insert_right(1)
 print(r.get_right_child())
 print(r.get_left_child())
 print(r.get_left_child().get_root_val())
